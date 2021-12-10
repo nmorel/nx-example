3a33ad8296b32c5cb2d958eb2dc268cf6a2df269
Nicolas Morel <nicolas.morel@klaxoon.com>
2021-12-10 16:16:21 +0100

    initial commit

---
+++ apps/klaxoon/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="klaxoon" />
+      <div />
+    </>
+  );
+}
+
+export default App;
